506ab6acc6ca2e2f7e1a66706494ae59378b10ab
vardhamani <121101210+vardhamani@users.noreply.github.com>
2023-08-25 01:46:51 +0530

    Update cloudformation.tf

--- cloudformation.tf
+++ cloudformation.tf
@@ -1,22 +1,10 @@
-AWSTemplateFormatVersion: '2010-09-09'
-Description: A template for creating a Route 53 hosted zone and alias record for example.com
-
 Resources:
-  MyHostedZone:
-    Type: AWS::Route53::HostedZone
-    Properties:
-      Name: "example.com." # Your domain name
-
   CloudFrontDNSRecord:
     Type: AWS::Route53::RecordSet
     Properties:
-      HostedZoneId: !Ref MyHostedZone
+      HostedZoneId: "YOUR_EXISTING_HOSTED_ZONE_ID" # Replace with your existing hosted zone ID
       Name: "example.com." # Your domain name
       Type: "A"
       AliasTarget:
         DNSName: !GetAtt CloudFrontDistribution.DomainName # CloudFront domain name
         HostedZoneId: "Z2FDTNDATAQYW2" # Hosted Zone ID for CloudFront
-
-Outputs:
-  HostedZone:
-    Value: !Ref MyHostedZone
