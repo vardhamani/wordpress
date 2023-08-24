a83406b6e5e37b8bf5fee86c0255c0a3a26c2a48
vardhamani <121101210+vardhamani@users.noreply.github.com>
2023-08-25 02:04:34 +0530

    Update cloudformation.tf

--- cloudformation.tf
+++ cloudformation.tf
@@ -1,10 +1,7 @@
-Resources:
-  CloudFrontDNSRecord:
-    Type: AWS::Route53::RecordSet
+  Resources:
+  ExistingCloudFrontDistribution:
+    Type: AWS::CloudFront::Distribution
     Properties:
-      HostedZoneId: "YOUR_EXISTING_HOSTED_ZONE_ID" # Replace with your existing hosted zone ID
-      Name: "example.com." # Your domain name
-      Type: "A"
-      AliasTarget:
-        DNSName: !GetAtt CloudFrontDistribution.DomainName # CloudFront domain name
-        HostedZoneId: "Z2FDTNDATAQYW2" # Hosted Zone ID for CloudFront
+      DistributionConfig:
+        Id: E1234567890EXAMPLE # Replace with your CloudFront distribution ID
+        WebACLId: 'arn:aws:wafv2:region:account-id:global/webacl/webacl-id' # Replace with your WebACL ARN
